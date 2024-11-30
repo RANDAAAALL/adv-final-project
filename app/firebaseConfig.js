9d4cfe76db586c1aeea39b9e2ae70b192d4d870f
RANDAAAALL <jenniecruz0506@gmail.com>
2024-12-01 02:28:10 +0800

    Add .env.local file for Firebase configuration

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,22 +1,20 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; 
 import { getFirestore } from "firebase/firestore"; 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBGYo1m2oDBAxMg6Ev0GsmY3GxdykGKmMg",
-  authDomain: "adv-final-project-714d3.firebaseapp.com",
-  projectId: "adv-final-project-714d3",
-  storageBucket: "adv-final-project-714d3.appspot.com", 
-  messagingSenderId: "116534471151",
-  appId: "1:116534471151:web:f0dd1721889f06d784ee1a",
-};
+require('dotenv').config();
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET, 
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-
-// export all the const
 export { app, auth, db };
